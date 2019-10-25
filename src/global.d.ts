bc49bf2341544c1bae46c9dcb6d416312c6e1a72
lipemat <mat@matlipe.com>
2019-10-25 12:05:46 -0400

    Inital structure pre debugging

---
+++ src/global.d.ts
@@ -0,0 +1,14 @@
+/**
+ * This is needed to allow ReactN to map the global state.
+ * Your app must have this available for typescript to compile.
+ *
+ * @link https://www.npmjs.com/package/reactn#typescript-support
+ */
+import 'reactn';
+
+declare module 'reactn/default' {
+	export interface State {
+		example: string;
+		keys: number;
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import produce, {Draft} from 'immer';
+import {useCallback} from 'react';
+import {getGlobal, setGlobal, useGlobal} from 'reactn';
+import {State} from 'reactn/default';
+
+// Use property of Global State
+export function useImmerGlobal<S extends keyof State>( property: S ):
+	[ State[S], ( f: ( draft: Draft<State[S]> ) => void | State[S] ) => void ];
+
+/**
+ * UseImmer for Global State
+ *
+ * @param {string} property
+ */
+export function useImmerGlobal( property ) {
+	const [ val, updateValue ] = useGlobal( property );
+
+	return [ val, useCallback( updater => {
+		updateValue( produce( getGlobal()[ property ], updater ) );
+	}, [ property, updateValue ] ) ];
+}
+
+// Use property of Global State
+export function setGlobalImmer<S extends keyof State>( property: S, updater: ( draft: Draft<State[S]> ) => void | State[S] ): Promise<State[S]>;
+
+/**
+ * Set Global State with Immer.
+ *
+ * @param {string} property
+ * @param {Function} updater
+ */
+export function setGlobalImmer( property, updater ) {
+	return setGlobal( {
+		[ property ]: produce( getGlobal()[ property ], updater )
+	} );
+}
